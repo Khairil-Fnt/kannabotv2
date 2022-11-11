cb0b672aa6ffff8e223c6e3bbf823077e0251f6d
ᴹᴿᤎ KhairilOfcི ࿇ <90993998+Khairil-Fnt@users.noreply.github.com>
2022-11-11 08:20:23 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
-*ᴀɢᴇ:* 20ᵗʰ
+*ɴᴀᴍᴇ:* Khairil春
+*ᴀɢᴇ:* 14ᵗʰ
 *sᴛᴀᴛᴜs:* single 
 
-*ʙɪʀᴛʜᴅᴀʏ:* 21 Des 2002
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bandung
+*ʙɪʀᴛʜᴅᴀʏ:* 17 Jun 2008
+*ᴀᴅᴅʀᴇss:* Malaysia, Sabah, Tawau
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
